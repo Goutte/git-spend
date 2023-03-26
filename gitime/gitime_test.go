9bcf373baff982bc0d6fb1b7bd7cca8faa18ec5c
Goutte <antoine@goutenoir.com>
2023-03-26 04:03:06 +0200

    config(coverage)
    /spend 30min

--- gitime/gitime_test.go
+++ gitime/gitime_test.go
@@ -16,8 +16,15 @@ type CollectTestDatum struct {
 	Message  string              `yaml:"message"`
 	Expected CollectTestExpected `yaml:"expected"`
 }
+
+// CollectTestExpected uses pointers, to handle missing values gracefully
 type CollectTestExpected struct {
-	Minutes uint64 `yaml:"minutes"`
+	Minutes *uint64 `yaml:"minutes"`
+	Hours   *uint64 `yaml:"hours"`
+	Days    *uint64 `yaml:"days"`
+	Weeks   *uint64 `yaml:"weeks"`
+	Months  *uint64 `yaml:"months"`
+	String  *string `yaml:"string"`
 }
 
 func TestCollectTimeSpent(t *testing.T) {
@@ -33,8 +40,35 @@ func TestCollectTimeSpent(t *testing.T) {
 
 	for _, tt := range testFile.Collect {
 		t.Run(tt.Name, func(t *testing.T) {
-			if got := CollectTimeSpent(tt.Message).ToMinutes(); got != tt.Expected.Minutes {
-				t.Errorf("CollectTimeSpent(%s) = %v, want %v", tt.Message, got, tt.Expected.Minutes)
+			if tt.Expected.Minutes != nil {
+				if got := CollectTimeSpent(tt.Message).ToMinutes(); got != *tt.Expected.Minutes {
+					t.Errorf("CollectTimeSpent(%s).ToMinutes() = %v, want %v", tt.Message, got, *tt.Expected.Minutes)
+				}
+			}
+			if tt.Expected.Hours != nil {
+				if got := CollectTimeSpent(tt.Message).ToHours(); got != *tt.Expected.Hours {
+					t.Errorf("CollectTimeSpent(%s).ToHours() = %v, want %v", tt.Message, got, *tt.Expected.Hours)
+				}
+			}
+			if tt.Expected.Days != nil {
+				if got := CollectTimeSpent(tt.Message).ToDays(); got != *tt.Expected.Days {
+					t.Errorf("CollectTimeSpent(%s).ToDays() = %v, want %v", tt.Message, got, *tt.Expected.Days)
+				}
+			}
+			if tt.Expected.Weeks != nil {
+				if got := CollectTimeSpent(tt.Message).ToWeeks(); got != *tt.Expected.Weeks {
+					t.Errorf("CollectTimeSpent(%s).ToWeeks() = %v, want %v", tt.Message, got, *tt.Expected.Weeks)
+				}
+			}
+			if tt.Expected.Months != nil {
+				if got := CollectTimeSpent(tt.Message).ToMonths(); got != *tt.Expected.Months {
+					t.Errorf("CollectTimeSpent(%s).ToMonths() = %v, want %v", tt.Message, got, *tt.Expected.Months)
+				}
+			}
+			if tt.Expected.String != nil {
+				if got := CollectTimeSpent(tt.Message).Normalize().String(); got != *tt.Expected.String {
+					t.Errorf("CollectTimeSpent(%s).Normalize().String() = %v, want %v", tt.Message, got, *tt.Expected.String)
+				}
 			}
 		})
 	}
