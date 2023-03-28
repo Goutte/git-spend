6517d7fc29742f6d41af75786afe957cc461eb30
Goutte <antoine@goutenoir.com>
2023-03-28 18:08:24 +0200

    feat: review and tweak the grammar
    /spend 1h

--- gitime/grammar.go
+++ gitime/grammar.go
@@ -2,19 +2,20 @@ package gitime
 
 import "regexp"
 
-var commandRegex = "^/spen[dt]\\s*"
+var commandRegex = "^\\s*/spen[dt]\\s*"
 var floatRegex = "[0-9]+[.]?[0-9]*|[0-9]*[.]?[0-9]+"
 
 // no negative lookahead in regexp, so we hack around it (to ignore datetime suffix)
-var minutesRegex = "(?P<minutes>" + floatRegex + ")\\s*(minutes?|mins?|mi?)?([^-/0-9]|$)"
-var hoursRegex = "(?P<hours>" + floatRegex + ")\\s*(hours?|ho?)\\s*"
-var daysRegex = "(?P<days>" + floatRegex + ")\\s*(days?|da?)\\s*"
-var weeksRegex = "(?P<weeks>" + floatRegex + ")\\s*(weeks?|we?)\\s*"
-var monthsRegex = "(?P<months>" + floatRegex + ")\\s*(months?|mo)\\s*"
-var miP = "(" + minutesRegex + ")?"
-var hoP = "(" + hoursRegex + ")?"
-var daP = "(" + daysRegex + ")?"
-var weP = "(" + weeksRegex + ")?"
-var moP = "(" + monthsRegex + ")?"
+// there's also regexp2, but its API needs some more work at the time of this writing
+var minutesRegex = "(?P<minutes>" + floatRegex + ")\\s*(?:minutes?|mins?|mi?)?([^-/0-9]|$)"
+var hoursRegex = "(?P<hours>" + floatRegex + ")\\s*(?:hours?|ho?)\\s*"
+var daysRegex = "(?P<days>" + floatRegex + ")\\s*(?:days?|da?)\\s*"
+var weeksRegex = "(?P<weeks>" + floatRegex + ")\\s*(?:weeks?|we?)\\s*"
+var monthsRegex = "(?P<months>" + floatRegex + ")\\s*(?:months?|mo)\\s*"
+var miP = "(?:" + minutesRegex + ")?"
+var hoP = "(?:" + hoursRegex + ")?"
+var daP = "(?:" + daysRegex + ")?"
+var weP = "(?:" + weeksRegex + ")?"
+var moP = "(?:" + monthsRegex + ")?"
 
 var spentAllRegex = regexp.MustCompile(commandRegex + moP + weP + daP + hoP + miP)
