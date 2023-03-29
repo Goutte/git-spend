5bac8ddbb25751c433276b56b282b33a97f3b8cc
Goutte <antoine@goutenoir.com>
2023-03-29 02:18:48 +0200

    refacto: remove legacy code
    /spend 2m

--- gitime/reader/stdin.go
+++ gitime/reader/stdin.go
@@ -3,33 +3,10 @@ package reader
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
-func DoesStdinHaveData() bool {
-	fileInfo, err := os.Stdin.Stat()
-	if err != nil {
-		return false
-	}
-
-	if os.Getenv("GIT_SPEND_NO_STDIN") == "1" {
-		return false
-	}
-
-	// Both yield false positives in CI, careful
-	//if (fileInfo.Mode() & os.ModeCharDevice) == 0 { // alternatively?
-	if (fileInfo.Mode() & os.ModeNamedPipe) != 0 {
-		return true
-	}
-
-	return false
-}
-
 func ReadStdin() string {
-	stdin, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
-	}
+	stdin, _ := io.ReadAll(os.Stdin)
 	return fmt.Sprintf("%s", stdin)
 }
